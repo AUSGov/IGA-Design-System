92b44ac839b09f7329b17e755277c69b84959030
leolinardi <leonardo_linardi@yahoo.com>
2022-06-23 12:43:59 +1000

    secondary footer, entity bar done!

--- src/.vuepress/theme/layouts/DoiContentHeadersFooters.vue
+++ src/.vuepress/theme/layouts/DoiContentHeadersFooters.vue
@@ -56,6 +56,16 @@
       </div>
     </nav>
 
+    <div class="entity-bar bg-aus-industry-participation-authority">
+      <span>Emissions reduction Assurance Committee</span>
+    </div>
+    <div class="entity-bar bg-industry-innovation-science">
+      <span>Industry Innovation and Science Australia Corporate</span>
+    </div>
+    <div class="entity-bar bg-inspiring-aus text-black">
+      <span>National Measurement Institute</span>
+    </div>
+
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
       <div class="carousel-controls">
         <div data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
@@ -179,6 +189,73 @@
         </div>
       </div>
     </div>
+
+    <div class="secondary-footer secondary-footer--extended-contact secondary-footer--connect-with-us secondary-footer--subscribe-updates">
+      <div class="container">
+        <div class="row main-row">
+          <div class="col-md base-contact">
+            <div class="h4">Contact Us</div>
+            <div class="mb-4 d-flex">
+              <span class="icon" v-html="email"></span>
+              <div class="d-flex flex-column">
+                <span class="body-large-bold">General Enquiries</span>
+                <a href="mailto:enquiries@space.gov.au">enquiries@space.gov.au</a>
+                <span class="body-large-bold">Media Enquiries</span>
+                <a href="mailto:enquiries@space.gov.au">enquiries@space.gov.au</a>
+              </div>
+            </div>
+            <div class="d-flex">
+              <span class="icon" v-html="phone"></span>
+              <div class="d-flex flex-column">
+                <a href="tel:+61 1800 497 182">1800 497 182 (within Australia)</a>
+                <a href="tel:+61 2 6276 1166">+61 2 6276 1166</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md extended-contact">
+            <div class="h4 h4-placeholder"></div>
+            <div class="mb-4 d-flex">
+              <span class="icon" v-html="mailbox"></span>
+              <div class="d-flex flex-column">
+                <span>Australian Space Agency,</span>
+                <span>GPO Box 2013,</span>
+                <span>CANBERRA ACT 2601</span>
+              </div>
+            </div>
+            <div class="d-flex">
+              <span class="icon" v-html="location"></span>
+              <div class="d-flex flex-column">
+                <span>McEwin Building.</span>
+                <span>Lot Fourteen, North Terrace,</span>
+                <span>Adelaide SA 5000</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md connect-with-us">
+            <div class="h4">Connect with us</div>
+            <ul>
+              <li class="mb-4">
+                <span class="icon" v-html="twitter"></span>
+                <span class="caption-large">Twitter</span>
+              </li>
+              <li class="mb-4">
+                <span class="icon" v-html="linkedin"></span>
+                <span class="caption-large">LinkedIn</span>
+              </li>
+              <li>
+                <span class="icon" v-html="facebook"></span>
+                <span class="caption-large">Facebook</span>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md subscribe-updates">
+            <div class="h4">Subscribe for updates</div>
+            <span class="icon" v-html="notification"></span>
+            <span class="caption-large">Subscribe for the latest news</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -192,11 +269,14 @@
   import twitter from '../../public/icons/twitter.svg?raw'
   import facebook from '../../public/icons/facebook.svg?raw'
   import instagram from '../../public/icons/instagram.svg?raw'
+  import linkedin from '../../public/icons/linkedin.svg?raw'
   import youtube from '../../public/icons/youtube.svg?raw'
   import externalLink from '../../public/icons/external-link.svg?raw'
   import linkArrowRight from '../../public/icons/link-arrow-right.svg?raw'
   import chevronBack from '../../public/icons/chevron-back.svg?raw'
   import chevronForward from '../../public/icons/chevron-forward.svg?raw'
+  import mailbox from '../../public/icons/mailbox.svg?raw'
+  import notification from '../../public/icons/notification.svg?raw'
 
   export default {
     data () {
@@ -210,11 +290,14 @@
         twitter,
         facebook,
         instagram,
+        linkedin,
         youtube,
         externalLink,
         linkArrowRight,
         chevronBack,
-        chevronForward
+        chevronForward,
+        mailbox,
+        notification
       }
     }
   }
