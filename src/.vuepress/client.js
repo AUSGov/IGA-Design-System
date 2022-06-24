863fe7910acd151fc0ca252f89a4180c8869fdec
WinstonLie <winstonl3900@gmail.com>
2022-06-24 17:38:33 +1000

    Working on filter component

--- src/.vuepress/client.js
+++ src/.vuepress/client.js
@@ -3,6 +3,7 @@ import ParentPage from './theme/layouts/ParentPage.vue'
 import DoiContent from './theme/layouts/DoiContent.vue'
 import DoiContentHeadersFooters from './theme/layouts/DoiContentHeadersFooters.vue'
 import DoiContentBanners from './theme/layouts/DoiContentBanners.vue'
+import DoiContentFilters from './theme/layouts/DoiContentFilters.vue'
 
 import 'bootstrap/dist/js/bootstrap.min'
 
@@ -12,5 +13,6 @@ export default defineClientConfig({
     app.component('DoiContent', DoiContent)
     app.component('DoiContentHeadersFooters', DoiContentHeadersFooters)
     app.component('DoiContentBanners', DoiContentBanners)
+    app.component('DoiContentFilters', DoiContentFilters)
   },
 })
---
+++ src/.vuepress/theme/layouts/DoiContentFilters.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="doi-content">
+    <nav class="filters">
+      <div class="title-wrapper-container">
+        <span class="title-container border-end border-2 lead">Entities</span>
+      </div>
+      <ul class="nav nav-tabs heading-extra-small">
+        <li class="nav-item">
+          <a aria-current="page" href="#">Anti-Dumping Commission</a>
+        </li>
+        <li class="nav-item">
+          <a aria-current="page" href="#">Anti-Dumping Review Panel</a>
+        </li>
+        <li class="nav-item">
+          <a aria-current="page" href="#">Ausindustry</a>
+        </li>
+        <li class="nav-item">
+          <a aria-current="page" href="#">Australian Industry Participation Authority</a>
+        </li>
+        <li class="nav-item">
+          <a aria-current="page" href="#">Australian SKA Office</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<script>
+
+</script>
