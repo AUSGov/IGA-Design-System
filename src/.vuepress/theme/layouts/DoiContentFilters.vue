20018fd25cebacd015d74ac867bde9537e9b578f
WinstonLie <winstonl3900@gmail.com>
2022-06-27 17:43:26 +1000

    Showing filters are mostly working. Needs further styling on select boxes and range sliders

--- src/.vuepress/theme/layouts/DoiContentFilters.vue
+++ src/.vuepress/theme/layouts/DoiContentFilters.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="doi-content">
+  <div class="doi-content mb-5">
     <nav class="filters">
       <div class="title-wrapper-container">
         <span class="title-container border-end border-2 lead">Entities</span>
@@ -67,13 +67,166 @@
       </div>
     </nav>
   </div>
+  <div class="doi-content m-lg-3">
+    <div class="show-filters">
+      <div class="buttons-container heading-small">
+        <span class="icon" v-html="filters"></span>
+        <button type="button" class="button-plain" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <span class="main-button-text">Show Filters</span>
+          <span class="icon chevron-right" v-html="chevronRight"></span>
+          <span class="icon chevron-down" v-html="chevronDown"></span>
+        </button>
+      </div>
+      <div id="collapseOne" class="main-accordion accordion-collapse collapse show">
+        <div class="filters-dropdown">
+          <div class="filters-dropdown-container">
+            <div class="buttons-container heading-small">
+              <button type="button" class="button-plain" data-bs-toggle="collapse" data-bs-target="#collapseTopic" aria-expanded="true" aria-controls="collapseToggle">
+                <span class="icon" v-html="circleAdd"></span>
+                <span class="main-button-text">Topic</span>
+              </button>
+            </div>
+            <div id="collapseTopic" class="accordion-collapse collapse accordion-content">
+              <select class="form-select mb-3">
+                <option selected>Choose a topic</option>
+                <option value="1">Australian Industry Participation Authority</option>
+                <option value="2">Anti-Dumping Commission</option>
+                <option value="3">AusIndustry</option>
+                <option value="3">Australian Industry Participation Authority...</option>
+                <option value="3">Australian Radioactive Waste Authority</option>
+              </select>
+
+            </div>
+          </div>
+        </div>
+        <div class="filters-dropdown">
+          <div class="filters-dropdown-container">
+            <div class="buttons-container heading-small">
+              <button type="button" class="button-plain" data-bs-toggle="collapse" data-bs-target="#collapseEntity" aria-expanded="true" aria-controls="collapseToggle">
+                <span class="icon" v-html="circleAdd"></span>
+                <span class="main-button-text">Entity</span>
+              </button>
+            </div>
+            <div id="collapseEntity" class="accordion-collapse collapse accordion-content">
+              <div class="form-check">
+                <div class="pb-1">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckFirst">
+                  <label class="form-check-label" for="flexCheckFirst">
+                    Australian Industry Participation  Authority
+                  </label>
+                </div>
+                <div class="pb-1">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckSecond">
+                  <label class="form-check-label" for="flexCheckSecond">
+                    Anti-Dumping Commission
+                  </label>
+                </div>
+                <div class="pb-1">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckThird">
+                  <label class="form-check-label" for="flexCheckThird">
+                    AusIndustry
+                  </label>
+                </div>
+                <div class="pb-1">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckFourth">
+                  <label class="form-check-label" for="flexCheckFourth">
+                    Australian Radioactive Waste Authority
+                  </label>
+                </div>
+                <div class="pb-1">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckFifth">
+                  <label class="form-check-label" for="flexCheckFifth">
+                    Australian SKA Office
+                  </label>
+                </div>
+                <div class="pb-1">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckSixth">
+                  <label class="form-check-label" for="flexCheckSixth">
+                    Australian Space Agency
+                  </label>
+                </div>
+                <div class="pb-1">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckSeventh">
+                  <label class="form-check-label" for="flexCheckSeventh">
+                    Critical Minerals Facilitation Office
+                  </label>
+                </div>
+                <div class="pb-1">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckEight">
+                  <label class="form-check-label" for="flexCheckEight">
+                    National Mesurement Institute
+                  </label>
+                </div>
+                <div class="pb-1">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckNinth">
+                  <label class="form-check-label" for="flexCheckNinth">
+                    Office of the Chief Economist
+                  </label>
+                </div>
+                <div class="pb-1">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckTenth">
+                  <label class="form-check-label" for="flexCheckTenth">
+                    Office of the Special Adviser to the Australian Government on Low Emissions Technology
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="filters-dropdown">
+          <div class="filters-dropdown-container">
+            <div class="buttons-container heading-small">
+              <button type="button" class="button-plain" data-bs-toggle="collapse" data-bs-target="#collapseCategory" aria-expanded="true" aria-controls="collapseToggle">
+                <span class="icon" v-html="circleAdd"></span>
+                <span class="main-button-text">Category</span>
+              </button>
+            </div>
+            <div id="collapseCategory" class="accordion-collapse collapse accordion-content">
+              <div class="dropdown">
+                <button type="button" class="topic-container button-plain" id="categoriesMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                  <span class="dropdown-title caption-large">All Categories</span>
+                  <span class="icon chevron-down" v-html="chevronDown"></span>
+                </button>
+                <div class="dropdown-menu" aria-labelledby="topicMenu">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="filters-dropdown">
+          <div class="filters-dropdown-container">
+            <div class="buttons-container heading-small">
+              <button type="button" class="button-plain" data-bs-toggle="collapse" data-bs-target="#collapseYear" aria-expanded="true" aria-controls="collapseToggle">
+                <span class="icon" v-html="circleAdd"></span>
+                <span class="main-button-text">Year</span>
+              </button>
+            </div>
+            <div id="collapseYear" class="accordion-collapse collapse accordion-content">
+              <div>
+                <input type="range" class="form-range" min="0" max="5" id="customRange2">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
   import chevronRight from '../../public/icons/chevron-right.svg?raw'
+  import chevronDown from '../../public/icons/chevron-down.svg?raw'
+  import filters from '../../public/icons/filters.svg?raw'
+  import circleAdd from '../../public/icons/circle-add.svg?raw'
   export default {
     data() {
       return {
-        chevronRight
+        chevronRight,
+        chevronDown,
+        filters,
+        circleAdd
       }
     }
   }
