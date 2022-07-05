36f2c5a71ab673d1d0fce361b1f4b2f88b87ac07
leolinardi <leonardo_linardi@yahoo.com>
2022-07-05 17:43:51 +1000

    refactor table css, finish news homepage

--- src/.vuepress/theme/layouts/components/TagTabsFilter.vue
+++ src/.vuepress/theme/layouts/components/TagTabsFilter.vue
@@ -1,63 +1,12 @@
 <template>
   <nav class="filters">
     <div class="title-wrapper-container">
-      <span class="title-container border-end border-2 lead">Entities</span>
+      <span class="title-container border-end border-2 lead">{{ title }}</span>
     </div>
     <div class="nav-group">
       <ul class="nav nav-tabs heading-extra-small">
-        <li class="nav-item">
-          <a aria-current="page" href="#">Anti-Dumping Commission</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Anti-Dumping Review Panel</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Ausindustry</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Australian Industry Participation Authority</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Australian SKA Office</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Australian SKA Office</a>
-        </li>
-      </ul>
-    </div>
-    <div class="icon-container">
-      <span class="icon" v-html="chevronForward"></span>
-    </div>
-  </nav>
-  <nav class="filters mb-5">
-    <div class="title-wrapper-container">
-      <span class="title-container border-end border-2 lead">Topics</span>
-    </div>
-    <div class="nav-group">
-      <ul class="nav nav-tabs heading-extra-small">
-        <li class="nav-item">
-          <a aria-current="page" href="#">Business</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Building and construction</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Corporate</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Emissions reductions</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Energy</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Government to Government</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Manufacturing</a>
-        </li>
-        <li class="nav-item">
-          <a aria-current="page" href="#">Manufacturing</a>
+        <li v-for="content in contents" class="nav-item">
+          <a aria-current="page" href="#">{{ content }}</a>
         </li>
       </ul>
     </div>
@@ -68,4 +17,15 @@
 </template>
 <script setup>
   import chevronForward from '../../../public/icons/chevron-forward.svg?raw'
+
+  const props = defineProps({
+    title: {
+      type: String,
+      required: true,
+    },
+    contents: {
+      type: Array,
+      required: true
+    },
+  })
 </script>
